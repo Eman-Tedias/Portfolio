a78eed989dda460a854de6bc57c01bd22f444e05
Eman <emanuelturibio@hotmail.com>
2025-06-24 09:35:36 -0300

    Implementacao em ambiente vite para deploy

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import VSCodePortfolio from './components/VSCodePortfolio';
+
+function App() {
+  return (
+    <div className="App">
+      <VSCodePortfolio />
+    </div>
+  );
+}
+
+export default App;
